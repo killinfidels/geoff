da23744117aa22bc34b147e138d4b6de4d9dd2ad
Mahdi Thaer Romman <mahdi0800@gmail.com>
2018-01-18 14:35:08 +0000

    adding geoff1

---
+++ geoff1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, result;
+
+    cout << "Please enter a number" << endl;
+
+    cin >> num1;
+
+    cout << "Please enter a second number" << endl;
+
+    cin >> num2;
+
+    result = num1 + num2;
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
